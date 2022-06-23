82a4d35f01d1c6fc5b1b4984cdf614a315285a40
Nepomniashchii <albertnepomnyashiy@gmail.com>
2022-06-24 00:29:13 +0700

    Makefile install uninstall

--- Calculator/main.c
+++ Calculator/main.c
@@ -5,17 +5,16 @@
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
-        fprintf(stderr, "No expression");
+        fprintf(stderr, "No expression" );
         exit(EXIT_FAILURE);
     }
     if (argc > 2) {
         fprintf(stderr, "Too many arguments");
         exit(EXIT_SUCCESS);
     }
-
-    char booba[BUFFER_SIZE];
-    booba[0] = '\0';
-    to_postfix_notation(argv[1], booba);
-    fprintf(stdout,"%f\n", calculate_postfix_notation(booba));
+    char input[BUFFER_SIZE];
+    input[0] = '\0';
+    to_postfix_notation(argv[1], input);
+    fprintf(stdout,"%f\n", calculate_postfix_notation(input));
     exit(EXIT_SUCCESS);
 }
